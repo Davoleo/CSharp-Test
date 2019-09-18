4d57e70ebc41f7ac3fac98189a9c98f991533680
Davoleo <davoleo2001@gmail.com>
2019-09-18 20:17:06 +0200

    Basics: Comments | Console Output | Read User input | Data Types

---
+++ Console-Test/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Example Single-line Comment
+            /* Example 
+             * Multiline
+             * Comment
+             */
+
+
+            //Hello World
+            Console.WriteLine("Hello World");
+
+            //Get user input
+            Console.Write("What's your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            //Data Types
+            bool canVote = true;
+            char fumoffu = '%';
+
+            int maxInt = int.MaxValue;
+            long maxLong = long.MaxValue;
+            decimal maxDecimal = decimal.MaxValue;
+            float maxFloat = float.MaxValue;
+            double maxDouble = double.MaxValue;
+
+            Console.WriteLine("Max Int: " + maxInt);
+
+            //Implicit type variable declaration
+            var sampleVar = "SampleString";
+
+            //You can't change its type after declaration though
+            //sampleVar = 2;
+
+            Console.WriteLine("sampleVar is a {0}", sampleVar.GetTypeCode());
+
+        }
+    }
+}
