82d66d6a07101917674cc8d28c034a532cb5efd1
Davoleo <davoleo2001@gmail.com>
2019-09-24 22:10:10 +0200

    Arithmetics | Casting | Random Numbers

--- Console-Test/Program.cs
+++ Console-Test/Program.cs
@@ -45,6 +45,47 @@ namespace Console_Test
             //sampleVar = 2;
 
             Console.WriteLine("sampleVar is a {0}", sampleVar.GetTypeCode());
+            Console.WriteLine("-------------------------------------------------------");
+
+            //Arithmetics
+            Console.WriteLine("5 + 3 = " + (5 + 3));
+            Console.WriteLine("5 - 3 = " + (5 - 3));
+            Console.WriteLine("5 * 3 = " + (5 * 3));
+            Console.WriteLine("5 / 3 = " + (5 / 3));
+            Console.WriteLine("5.2 % 3 = " + (5.2 % 3));
+
+            int i = 0;
+
+            Console.WriteLine("i++ = " + i++);
+            Console.WriteLine("++i = " + ++i);
+            Console.WriteLine("i-- = " + i--);
+            Console.WriteLine("--i = " + --i);
+
+            Console.WriteLine("-------------------------------------------------------");
+            //Some Math Static Functions
+            //acos, asin, atan, atan2, cos, cosh, exp, log, sin, sinh, tan, tanh
+            double num1 = 10.5;
+            double num2 = 15;
+
+            Console.WriteLine("Abs(num1): " + Math.Abs(num1));
+            Console.WriteLine("Ceiling(num1): " + Math.Ceiling(num1));
+            Console.WriteLine("Floor(num1): " + Math.Floor(num1));
+            Console.WriteLine("Max(num1, num2): " + Math.Max(num1, num2));
+            Console.WriteLine("Min(num1, num2): " + Math.Min(num1, num2));
+            Console.WriteLine("Pow(num1): " + Math.Pow(num1, num2));
+            Console.WriteLine("Round(num1): " + Math.Round(num1));
+            Console.WriteLine("Sqrt(num1): " + Math.Sqrt(num1));
+
+            Console.WriteLine("-------------------------------------------------------");
+
+            //Casting
+            const double PI = 3.14;
+            int intPI = (int)PI;
+
+            //Generating Random Numbers
+            Random rand = new Random();
+            Console.WriteLine("Random number between 1 and 10: " + rand.Next(1, 11));
+
 
         }
     }
