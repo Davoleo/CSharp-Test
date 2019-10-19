796ce3fedec76e2aa76e3c013dce74b37da2e369
Davoleo <davoleo2001@gmail.com>
2019-10-19 22:35:47 +0200

    SQL Server DB

---
+++ CSharp-Test/dbo.Athletes.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Athletes]
+(
+	[uid] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [name] VARCHAR(20) NULL, 
+    [age] INT NULL, 
+    [keyCountry] INT NULL, 
+    [taxCode] CHAR(16) NULL
+)
+
+GO
+
+
+CREATE INDEX [IX_Table_Column] ON [dbo].[Athletes] ([keyCountry])
